1e4419fbc025802f86cacd98aeecefe0c1c1baef
josias <kologojosias@gmail.com>
2019-09-20 12:45:44 +0100

    Updates

---
+++ precache-manifest.b5c1b2db502589e00c75afaff4625578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b4212f3579821302169e5c5abd8622",
+    "url": "/Josias1997/portfolio/index.html"
+  },
+  {
+    "revision": "b1fd3ff8850ba9118aee",
+    "url": "/Josias1997/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "d5e4bf34032c4ea02a48",
+    "url": "/Josias1997/portfolio/static/css/main.98ee337e.chunk.css"
+  },
+  {
+    "revision": "b1fd3ff8850ba9118aee",
+    "url": "/Josias1997/portfolio/static/js/2.243f33ef.chunk.js"
+  },
+  {
+    "revision": "d5e4bf34032c4ea02a48",
+    "url": "/Josias1997/portfolio/static/js/main.57eef103.chunk.js"
+  },
+  {
+    "revision": "ba4ce2d0ed8bbc9d962e",
+    "url": "/Josias1997/portfolio/static/js/runtime~main.2540eb1f.js"
+  }
+]);
